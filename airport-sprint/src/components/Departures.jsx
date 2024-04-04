7557644a0d98f388f6bc3c3d8921da595238229a
Matt Young <127060152+myoung24@users.noreply.github.com>
2024-04-04 10:02:39 -0230

    arrivals/departure pages working

--- airport-sprint/src/components/Departures.jsx
+++ airport-sprint/src/components/Departures.jsx
@@ -1,13 +1,87 @@
-import Header from "./Header"
-
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import axios from "axios";
 
 const Departures = () => {
+  const [airports, setAirports] = useState([]);
+  const [selectedAirport, setSelectedAirport] = useState("");
+  const [flightDetails, setFlightDetails] = useState([]);
+
+  useEffect(() => {
+    fetchAirports();
+  }, []);
+
+  const fetchAirports = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/airports");
+      setAirports(response.data);
+    } catch (error) {
+      console.error("Error fetching airports:", error);
+    }
+  };
+
+  const handleAirportChange = async (event) => {
+    const selectedAirportId = event.target.value;
+    setSelectedAirport(selectedAirportId);
+
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/flights/origin/${selectedAirportId}`
+      );
+      setFlightDetails(response.data); // Assuming this is an array of flights
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching flight details:", error);
+    }
+  };
+
   return (
     <div>
       <Header />
-      
-    </div>
-  )
-}
+      <h2>DEPARTURES</h2>
+      <br />
+      <h3>Airports:</h3>
 
-export default Departures
+      <select id="airportDropdown" onChange={handleAirportChange}>
+        <option value="">Select an airport</option>
+        {airports.map((airport) => (
+          <option key={airport.id} value={airport.id}>
+            {airport.name}
+          </option>
+        ))}
+      </select>
+      {selectedAirport && flightDetails.length > 0 && (
+        <div>
+          <h2>Flight Details</h2>
+          <table className="flight-table">
+            <thead>
+              <tr>
+                <th>Flight Number</th>
+                <th>To</th>
+                <th>Departure Time</th>
+                <th>Departure Gate</th>
+                <th>Arrival Time</th>
+                <th>Arrival Gate</th>
+                <th>Flight Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {flightDetails.map((flight, index) => (
+                <tr key={index}>
+                  <td>{flight.flightNumber}</td>
+                  <td>{flight.destinationAirport.name}</td>
+                  <td>{flight.departureTime}</td>
+                  <td>{flight.departureGate.gateNumber}</td>{" "}
+                  <td>{flight.arrivalTime}</td>
+                  <td>{flight.arrivalGate.gateNumber}</td>{" "}
+                  <td>{flight.flightStatus}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Departures;
--- airport-sprint/src/components/Header.jsx
+++ airport-sprint/src/components/Header.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
   return (
-  
-    <div className='header-container'>
+    <div className="header-container">
+      <Link to="/">
+        <img
+          src={require("./images/mapleview.png")}
+          alt="airplane-logo"
+          className="airport-logo"
+        />
+      </Link>
 
-        <Link to='/'>
-          <img src={require("./images/mapleview.png")} alt='airplane-logo' className='airport-logo'/>
-        </Link>
+      <hr className="separator" />
 
-        <hr className='separator' />
-
-        <nav>
-            <ul className='navigation'>
-            <li><Link to='/reservations'>RESERVATIONS</Link></li>
-            <li><Link to='/arrivals'>ARRIVALS / DEPARTURES</Link></li>
-            <li><Link to='/admin'>ADMIN</Link></li>
-            </ul>
-        </nav>
-        
+      <nav>
+        <ul className="navigation">
+          <li>
+            <Link to="/reservations">RESERVATIONS</Link>
+          </li>
+          <li>
+            <Link to="/arrivals">ARRIVALS</Link>
+          </li>
+          <li>
+            <Link to="/departures">DEPARTURES</Link>
+          </li>
+          <li>
+            <Link to="/admin">ADMIN</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
