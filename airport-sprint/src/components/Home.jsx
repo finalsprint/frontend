5e094282f303d0d41f61182b8b0affea7950b45a
saamhynes <samantha.hynes@keyin.com>
2024-04-08 17:10:23 -0300

    Landing page update

--- airport-sprint/src/components/Home.jsx
+++ airport-sprint/src/components/Home.jsx
@@ -3,10 +3,21 @@ import Admin from "./Admin"
 
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <>
+    <Header />
+    <div className="welcome-container">
+      
+      <h2 className="welcome-title">
+      Welcome to <br/>Mapleview International Airlines</h2>
+
+      <p className="welcome">We're thrilled to have you on board. Whether you're embarking on an adventure or heading back home, we're here to make your journey seamless.<br/> <br/>
+
+      Ready to explore? Navigate effortlessly to our Arrivals and Departures page to plan your next flight or manage your reservations.<br/> <br/>
+
+      Sit back, relax, and let Mapleview International Airlines take you where you need to go.</p>
       
     </div>
+    </>
   )
 }
 
